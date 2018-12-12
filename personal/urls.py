1ab04253fe7054adf5bbd0938fd1f73c841a5bc7
momo ruan <momoruan06@gmail.com>
2018-12-12 16:18:45 +0700

    first commit

---
+++ personal/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index,name='index'),
+    url(r'^contact/', views.contact,name='contact')
+]
+# Create your views here.
---
+++ personal/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html',{'content':['If you would like to contact me','momoruan06@gmail.com']})
